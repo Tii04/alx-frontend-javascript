cd2c92b85e123b03eeb13bf19e69ce807cf91899
Tii04 <tiisetso.qsebata@gmail.com>
2023-07-27 19:59:05 +0200

    Implement initialiseRooms function

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from './0-classroom';
 
-    export default function initializeRooms() {
+    export default function initializeRooms(){
         const c1 = new ClassRoom(19);
         const c2 = new ClassRoom(20);
         const c3 = new ClassRoom(34);
